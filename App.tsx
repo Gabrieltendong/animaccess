253e4b082b1fc6e5a234fa7a2c1466dc3bcd124c
Gabriel <gabitendong@gmail.com>
2024-03-05 16:51:13 +0100

    fix bug build ios

--- App.tsx
+++ App.tsx
@@ -1,14 +1,15 @@
-import 'react-native-gesture-handler';
+// import 'react-native-gesture-handler';
+import {Text, View} from 'react-native';
 import * as React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {QueryClient, QueryClientProvider} from 'react-query';
+// import {NavigationContainer} from '@react-navigation/native';
+// import {QueryClient, QueryClientProvider} from 'react-query';
 import SplashScreen from 'react-native-splash-screen';
-import Rooter from 'src/navigations';
-import {enableLatestRenderer} from 'react-native-maps';
+// import Rooter from 'src/navigations';
+// import {enableLatestRenderer} from 'react-native-maps';
 
-enableLatestRenderer();
+// enableLatestRenderer();
 
-const queryClient = new QueryClient();
+// const queryClient = new QueryClient();
 
 const App = () => {
   React.useEffect(() => {
@@ -18,11 +19,14 @@ const App = () => {
   }, []);
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <NavigationContainer>
-        <Rooter />
-      </NavigationContainer>
-    </QueryClientProvider>
+    // <QueryClientProvider client={queryClient}>
+    //   <NavigationContainer>
+    //     <Rooter />
+    //   </NavigationContainer>
+    // </QueryClientProvider>
+    <View style={{justifyContent: 'center', flex: 1, alignItems: 'center'}}>
+      <Text>Bonjour le monde</Text>
+    </View>
   );
 };
 
