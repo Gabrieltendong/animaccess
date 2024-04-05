cbff13074ca2270bf6a83b91b4eeb9ecf288f775
FlavienMedina <flavien_94@hotmail.fr>
2024-04-05 20:04:03 +0200

    switch scrollview to btn pour mes reservations

--- src/screens/etablissement/myaccount/index.jsx
+++ src/screens/etablissement/myaccount/index.jsx
@@ -1,13 +1,17 @@
 //import liraries
 import React, { Component, useEffect, useState } from 'react';
-import { View, Text, ScrollView } from 'react-native';
+import { TouchableOpacity, View, Text, ScrollView } from 'react-native';
+import Icon from '@components/ui/Icon';
 import { styles } from './styles'
 import Container from '@components/common/Container';
 import Accordion from 'react-native-collapsible/Accordion';
+import { colors } from "@themes/index";
 import MyInfos from '@components/MyInfos';
 import MyBooking from '@components/MyBooking';
 import SectionItem from '@components/SectionItem';
 import MyInfosEtablissement from '@components/MyInfosEtablissement';
+import { MYBOOKING_ETABLISSEMENT } from '@constants/routes';
+import { useNavigation } from '@react-navigation/native';
 import { useQuery } from 'react-query';
 import { useUserStore } from 'src/store/user.store';
 import { get_my_booking_etablissement } from 'src/feature/booking/booking.service';
@@ -19,11 +23,11 @@ const SECTIONS = [
       content: <MyInfosEtablissement />,
       icon: "UserRound"
     },
-    {
-      title: 'Mes réservations',
-      content: <MyBooking />,
-      icon: "CalendarCheck"
-    },
+    // {
+    //   title: 'Mes réservations',
+    //   content: <MyBooking />,
+    //   icon: "CalendarCheck"
+    // },
 ];
 
 const renderContent = ({content}) => {
@@ -36,7 +40,7 @@ const renderContent = ({content}) => {
 
 // create a component
 const MyAccountScreen = () => {
-
+    const navigation = useNavigation()
     const { user } = useUserStore()
     const { setListBookingEtablissement } = useBookingStore()
     const etablissement_id = user?.account?.id
@@ -49,7 +53,7 @@ const MyAccountScreen = () => {
    
     return (
         <Container title={"Mon compte"}>
-            <ScrollView contentContainerStyle={{paddingBottom: 100}}>
+            <View contentContainerStyle={{paddingBottom: 100}}>
                 <Accordion
                     sections={SECTIONS}
                     activeSections={activeSections}
@@ -60,7 +64,11 @@ const MyAccountScreen = () => {
                     underlayColor={'transparent'}
                     sectionContainerStyle={{marginVertical: 10}}
                 />
-            </ScrollView>
+                <TouchableOpacity onPress={() => navigation.navigate(MYBOOKING_ETABLISSEMENT)} style={styles.mybookingsBtn}>
+                    <Icon name={"CalendarCheck"} color={colors.BLACK} size={16} />
+                    <Text style={styles.mybookingsBtnText}>Mes réservations</Text>
+                </TouchableOpacity>
+            </View>
         </Container>
     );
 };
--- src/screens/etablissement/myaccount/styles.js
+++ src/screens/etablissement/myaccount/styles.js
@@ -1,4 +1,5 @@
 import { StyleSheet } from "react-native";
+import { colors, fonts } from "@themes/index";
 
 export const styles = StyleSheet.create({
     container: {
@@ -12,5 +13,17 @@ export const styles = StyleSheet.create({
         width: '95%',
         alignSelf: 'center',
         marginTop: 1
-    }
+    },
+    mybookingsBtn:{
+        flexDirection: 'row',
+        alignItems: 'center',
+        display:'flex',
+        backgroundColor: colors.WHITE,
+        borderRadius: 50,
+        paddingHorizontal: 20,
+        paddingVertical: 20
+    },
+    mybookingsBtnText: {
+        marginLeft: 10,
+    },
 })
